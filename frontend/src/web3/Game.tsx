387e134c928d6e3bb1e2a0bd263e1cc4febb83f4
TheMystic07 <gurshabad90@gmail.com>
2024-05-22 20:00:39 +0530

    Added Loading Bar to the game

--- frontend/src/web3/Game.tsx
+++ frontend/src/web3/Game.tsx
@@ -2,12 +2,25 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export const Game = () => {
-    const { unityProvider } = useUnityContext({
+    const { unityProvider ,  loadingProgression, isLoaded } = useUnityContext({
         loaderUrl: "/Build/GreenArrow.loader.js",
         dataUrl: "/Build/GreenArrow.data",
         frameworkUrl: "/Build/GreenArrow.framework.js",
         codeUrl: "/Build/GreenArrow.wasm",
       });
     
-      return <Unity unityProvider={unityProvider}  className="w-[70vw] rounded-2xl " />;
+      return (
+        <>
+          {!isLoaded && (
+            <p className="text-6xl text-white text-center m-2 p-2 " >Loading Application... {Math.round(loadingProgression * 100)}%</p>
+          )}
+          <Unity
+            unityProvider={unityProvider}
+            className="w-[70vw] rounded-2xl "
+            style={{ visibility: isLoaded ? "visible" : "hidden" }}
+          />
+        </>
+      );
     }
+
+
